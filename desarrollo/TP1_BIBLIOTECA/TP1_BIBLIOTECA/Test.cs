32509d27ddee72cfda6aef3ba1143d6126c3f287
adco23 <88953154+adco23@users.noreply.github.com>
2025-04-18 19:39:14 -0300

    Commit inicial

---
+++ desarrollo/TP1_BIBLIOTECA/TP1_BIBLIOTECA/Test.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using TP1_BIBLIOTECA.entidad;
+
+namespace TP1_BIBLIOTECA
+{
+    internal class Test
+    {
+        static void Main(string[] args)
+        {
+            Biblioteca biblioteca = new Biblioteca();
+
+            cargarLibros(10);
+            cargarLibros(5);
+
+            biblioteca.ListarLibros();
+            biblioteca.EliminarLibro("Libro5");
+            biblioteca.ListarLibros();
+
+            void cargarLibros(int cantidad)
+            {
+                bool pude;
+
+                for (int i = 1; i <= cantidad; i++)
+                {
+                    pude = biblioteca.AgregarLibro("Libro" + i, "Autor" + i, "Editorial" + i);
+                    if (pude) Console.WriteLine("Libro" + i + " agregado correctamente.");
+                    else Console.WriteLine("Libro" + i + " ya existe en la biblioteca.");
+                }
+            }
+        }
+    }
+}
