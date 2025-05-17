b2b6a687c0f6e283461ac51cf3d191f5fc4b5ffe
Marcela <marcela.cordini@gmail.com>
2025-05-17 11:46:49 -0300

    Agrego Vista Login

---
+++ PI_CLUB_DEPORTIVO/vistas/Login.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace PI_CLUB_DEPORTIVO.vistas
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtUsuario_Enter(object sender, EventArgs e)
+        {
+            /* este evento se ejecuta cuando llega el foco */
+
+            if (txtUsuario.Text == "Usuario")
+            {
+                txtUsuario.Text = "";
+            }
+        }
+
+        private void txtUsuario_Leave(object sender, EventArgs e)
+        {
+            /* este evento se ejecuta cuando se va el foco */
+
+            if (txtUsuario.Text == "")
+            {
+                txtUsuario.Text = "Usuario";
+            }
+        }
+
+        private void txtPass_Enter(object sender, EventArgs e)
+        {
+            if (txtPass.Text == "Contraseña")
+            {
+                txtPass.Text = "";
+                txtPass.UseSystemPasswordChar = true;
+            }
+        }
+
+        private void txtPass_Leave(object sender, EventArgs e)
+        {
+            if (txtPass.Text == "")
+            {
+                txtPass.Text = "Contraseña";
+                txtPass.UseSystemPasswordChar = false;
+            }
+        }
+
+        /*private void btnIngresar_Click(object sender, EventArgs e)
+        {
+            DataTable tablaLogin = new DataTable(); // es la que recibe los
+            datos desde el formulario
+            Datos.Usuarios dato = new Datos.Usuarios(); // variable que
+            contiene todas las caracteristicas de la clase
+            tablaLogin = dato.Log_Usu(txtUsuario.Text, txtPass.Text);
+            if (tablaLogin.Rows.Count > 0)
+            {
+                // quiere decir que el resultado tiene 1 fila por lo que el
+                usuario EXISTE
+            MessageBox.Show("Ingreso exitoso");
+            }
+            else
+            {
+                MessageBox.Show("Usuario y/o password incorrecto");
+            }
+        }*/
+    }
+}
