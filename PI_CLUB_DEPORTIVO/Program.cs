29ecd6db83fe3049cc452f10a7e171d8f6a5f354
adco23 <88953154+adco23@users.noreply.github.com>
2025-05-17 18:16:31 -0300

    FEAT: agrega formulario inicial

--- PI_CLUB_DEPORTIVO/Program.cs
+++ PI_CLUB_DEPORTIVO/Program.cs
@@ -1,3 +1,5 @@
+using PI_CLUB_DEPORTIVO.vistas;
+
 namespace PI_CLUB_DEPORTIVO
 {
     internal static class Program
@@ -11,7 +13,7 @@ namespace PI_CLUB_DEPORTIVO
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new Login());
         }
     }
 }
