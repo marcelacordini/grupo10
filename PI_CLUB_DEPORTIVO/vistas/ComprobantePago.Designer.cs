41281ffa5e7e55eab0183d886b324a55cc143f41
Marcela <marcela.cordini@gmail.com>
2025-06-18 19:43:04 -0300

    comprobante
    agrego comprobante de pago

---
+++ PI_CLUB_DEPORTIVO/vistas/ComprobantePago.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace PI_CLUB_DEPORTIVO.vistas
+{
+    partial class ComprobantePago
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            lblSociod = new Label();
+            lblSocio = new Label();
+            lblconceptod = new Label();
+            lblConcepto = new Label();
+            lblMontod = new Label();
+            lblMonto = new Label();
+            lblFechad = new Label();
+            lblFecha = new Label();
+            lblMedioPagod = new Label();
+            lblMedioPago = new Label();
+            lblComprobanted = new Label();
+            lblComprobante = new Label();
+            btnImprimir = new Button();
+            btnCerrar = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = SystemColors.ControlLightLight;
+            label1.Location = new Point(28, 58);
+            label1.Name = "label1";
+            label1.Size = new Size(217, 23);
+            label1.TabIndex = 2;
+            label1.Text = "COMPROBANTE DE PAGO";
+            // 
+            // lblSociod
+            // 
+            lblSociod.AutoSize = true;
+            lblSociod.ForeColor = SystemColors.ControlLightLight;
+            lblSociod.Location = new Point(24, 108);
+            lblSociod.Name = "lblSociod";
+            lblSociod.Size = new Size(78, 23);
+            lblSociod.TabIndex = 3;
+            lblSociod.Text = "CLIENTE:";
+            // 
+            // lblSocio
+            // 
+            lblSocio.AutoSize = true;
+            lblSocio.ForeColor = SystemColors.ControlLightLight;
+            lblSocio.Location = new Point(207, 108);
+            lblSocio.Name = "lblSocio";
+            lblSocio.Size = new Size(61, 23);
+            lblSocio.TabIndex = 4;
+            lblSocio.Text = "SOCIO";
+            // 
+            // lblconceptod
+            // 
+            lblconceptod.AutoSize = true;
+            lblconceptod.ForeColor = SystemColors.ControlLightLight;
+            lblconceptod.Location = new Point(24, 155);
+            lblconceptod.Name = "lblconceptod";
+            lblconceptod.Size = new Size(102, 23);
+            lblconceptod.TabIndex = 5;
+            lblconceptod.Text = "CONCEPTO:";
+            // 
+            // lblConcepto
+            // 
+            lblConcepto.AutoSize = true;
+            lblConcepto.ForeColor = SystemColors.ControlLightLight;
+            lblConcepto.Location = new Point(206, 155);
+            lblConcepto.Name = "lblConcepto";
+            lblConcepto.Size = new Size(98, 23);
+            lblConcepto.TabIndex = 6;
+            lblConcepto.Text = "CONCEPTO";
+            // 
+            // lblMontod
+            // 
+            lblMontod.AutoSize = true;
+            lblMontod.ForeColor = SystemColors.ControlLightLight;
+            lblMontod.Location = new Point(24, 208);
+            lblMontod.Name = "lblMontod";
+            lblMontod.Size = new Size(76, 23);
+            lblMontod.TabIndex = 7;
+            lblMontod.Text = "MONTO:";
+            // 
+            // lblMonto
+            // 
+            lblMonto.AutoSize = true;
+            lblMonto.ForeColor = SystemColors.ControlLightLight;
+            lblMonto.Location = new Point(207, 208);
+            lblMonto.Name = "lblMonto";
+            lblMonto.Size = new Size(72, 23);
+            lblMonto.TabIndex = 8;
+            lblMonto.Text = "MONTO";
+            // 
+            // lblFechad
+            // 
+            lblFechad.AutoSize = true;
+            lblFechad.ForeColor = SystemColors.ControlLightLight;
+            lblFechad.Location = new Point(24, 265);
+            lblFechad.Name = "lblFechad";
+            lblFechad.Size = new Size(65, 23);
+            lblFechad.TabIndex = 9;
+            lblFechad.Text = "FECHA:";
+            // 
+            // lblFecha
+            // 
+            lblFecha.AutoSize = true;
+            lblFecha.ForeColor = SystemColors.ControlLightLight;
+            lblFecha.Location = new Point(207, 265);
+            lblFecha.Name = "lblFecha";
+            lblFecha.Size = new Size(61, 23);
+            lblFecha.TabIndex = 10;
+            lblFecha.Text = "FECHA";
+            // 
+            // lblMedioPagod
+            // 
+            lblMedioPagod.AutoSize = true;
+            lblMedioPagod.ForeColor = SystemColors.ControlLightLight;
+            lblMedioPagod.Location = new Point(24, 324);
+            lblMedioPagod.Name = "lblMedioPagod";
+            lblMedioPagod.Size = new Size(129, 23);
+            lblMedioPagod.TabIndex = 11;
+            lblMedioPagod.Text = "Medio de Pago:";
+            // 
+            // lblMedioPago
+            // 
+            lblMedioPago.AutoSize = true;
+            lblMedioPago.ForeColor = SystemColors.ControlLightLight;
+            lblMedioPago.Location = new Point(207, 324);
+            lblMedioPago.Name = "lblMedioPago";
+            lblMedioPago.Size = new Size(125, 23);
+            lblMedioPago.TabIndex = 12;
+            lblMedioPago.Text = "Medio de Pago";
+            // 
+            // lblComprobanted
+            // 
+            lblComprobanted.AutoSize = true;
+            lblComprobanted.ForeColor = SystemColors.ControlLightLight;
+            lblComprobanted.Location = new Point(28, 381);
+            lblComprobanted.Name = "lblComprobanted";
+            lblComprobanted.Size = new Size(134, 23);
+            lblComprobanted.TabIndex = 13;
+            lblComprobanted.Text = "COMPROBANTE";
+            // 
+            // lblComprobante
+            // 
+            lblComprobante.AutoSize = true;
+            lblComprobante.ForeColor = SystemColors.ControlLightLight;
+            lblComprobante.Location = new Point(207, 381);
+            lblComprobante.Name = "lblComprobante";
+            lblComprobante.Size = new Size(134, 23);
+            lblComprobante.TabIndex = 14;
+            lblComprobante.Text = "COMPROBANTE";
+            // 
+            // btnImprimir
+            // 
+            btnImprimir.Location = new Point(556, 212);
+            btnImprimir.Name = "btnImprimir";
+            btnImprimir.Size = new Size(157, 52);
+            btnImprimir.TabIndex = 15;
+            btnImprimir.Text = "IMPRIMIR";
+            btnImprimir.UseVisualStyleBackColor = true;
+            btnImprimir.Click += btnImprimir_Click_1;
+            // 
+            // btnCerrar
+            // 
+            btnCerrar.Location = new Point(556, 309);
+            btnCerrar.Name = "btnCerrar";
+            btnCerrar.Size = new Size(157, 52);
+            btnCerrar.TabIndex = 16;
+            btnCerrar.Text = "CERRAR";
+            btnCerrar.UseVisualStyleBackColor = true;
+            btnCerrar.Click += btnCerrar_Click_1;
+            // 
+            // ComprobantePago
+            // 
+            AutoScaleDimensions = new SizeF(9F, 23F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.DimGray;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnCerrar);
+            Controls.Add(btnImprimir);
+            Controls.Add(lblComprobante);
+            Controls.Add(lblComprobanted);
+            Controls.Add(lblMedioPago);
+            Controls.Add(lblMedioPagod);
+            Controls.Add(lblFecha);
+            Controls.Add(lblFechad);
+            Controls.Add(lblMonto);
+            Controls.Add(lblMontod);
+            Controls.Add(lblConcepto);
+            Controls.Add(lblconceptod);
+            Controls.Add(lblSocio);
+            Controls.Add(lblSociod);
+            Controls.Add(label1);
+            ForeColor = SystemColors.ControlText;
+            Name = "ComprobantePago";
+            Text = "ComprobantePago";
+            Controls.SetChildIndex(label1, 0);
+            Controls.SetChildIndex(lblSociod, 0);
+            Controls.SetChildIndex(lblSocio, 0);
+            Controls.SetChildIndex(lblconceptod, 0);
+            Controls.SetChildIndex(lblConcepto, 0);
+            Controls.SetChildIndex(lblMontod, 0);
+            Controls.SetChildIndex(lblMonto, 0);
+            Controls.SetChildIndex(lblFechad, 0);
+            Controls.SetChildIndex(lblFecha, 0);
+            Controls.SetChildIndex(lblMedioPagod, 0);
+            Controls.SetChildIndex(lblMedioPago, 0);
+            Controls.SetChildIndex(lblComprobanted, 0);
+            Controls.SetChildIndex(lblComprobante, 0);
+            Controls.SetChildIndex(btnImprimir, 0);
+            Controls.SetChildIndex(btnCerrar, 0);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label lblSociod;
+        private Label lblSocio;
+        private Label lblconceptod;
+        private Label lblConcepto;
+        private Label lblMontod;
+        private Label lblMonto;
+        private Label lblFechad;
+        private Label lblFecha;
+        private Label lblMedioPagod;
+        private Label lblMedioPago;
+        private Label lblComprobanted;
+        private Label lblComprobante;
+        private Button btnImprimir;
+        private Button btnCerrar;
+    }
+}
---
+++ PI_CLUB_DEPORTIVO/vistas/ComprobantePago.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PI_CLUB_DEPORTIVO.vistas.baseForm;
+
+namespace PI_CLUB_DEPORTIVO.vistas
+{
+    public partial class ComprobantePago : BaseForm
+    {
+        public ComprobantePago(int ClienteId, string concepto, decimal monto, DateTime fecha, string medioPago)
+        {
+            InitializeComponent();
+
+            lblSocio.Text = ClienteId.ToString();
+            lblConcepto.Text = concepto;
+            lblMonto.Text = $"$ {monto:F2}";
+            lblFecha.Text = (DateTime.Now).ToString();
+            lblMedioPago.Text = medioPago;
+            lblComprobante.Text = GenerarNumeroComprobante();
+        }
+
+        private string GenerarNumeroComprobante()
+        {
+            return "CMP-" + DateTime.Now.ToString("yyyyMMddHHmmss");
+        }
+
+        private void btnImprimir_Click_1(object sender, EventArgs e)
+        {
+            PrintDocument doc = new PrintDocument();
+            doc.PrintPage += (s, ev) =>
+            {
+                ev.Graphics.DrawString($"Comprobante de Pago", new Font("Arial", 14, FontStyle.Bold), Brushes.Black, new Point(100, 40));
+                ev.Graphics.DrawString($"Socio: {lblSociod.Text}", new Font("Arial", 12), Brushes.Black, new Point(100, 80));
+                ev.Graphics.DrawString($"Concepto: {lblConcepto.Text}", new Font("Arial", 12), Brushes.Black, new Point(100, 110));
+                ev.Graphics.DrawString($"Fecha: {lblFecha.Text}", new Font("Arial", 12), Brushes.Black, new Point(100, 140));
+                ev.Graphics.DrawString($"Monto: {lblMonto.Text}", new Font("Arial", 12), Brushes.Black, new Point(100, 170));
+                ev.Graphics.DrawString($"Medio de Pago: {lblMedioPago.Text}", new Font("Arial", 12), Brushes.Black, new Point(100, 200));
+                ev.Graphics.DrawString($"Nro Comprobante: {lblComprobanted.Text}", new Font("Arial", 12), Brushes.Black, new Point(100, 230));
+            };
+
+            PrintDialog dialog = new PrintDialog();
+            dialog.Document = doc;
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                doc.Print();
+            }
+        }
+
+        private void btnCerrar_Click_1(object sender, EventArgs e)
+        {
+            MenuPrincipal principal = new MenuPrincipal();
+            principal.Show();
+            this.Hide();
+        }
+
+    }
+}
+
