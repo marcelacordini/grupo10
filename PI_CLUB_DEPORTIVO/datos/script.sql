3cfa917842046ea50f79d110404f7275f0694dcd
LucasMonteras <lucasmonteras@gmail.com>
2025-06-17 22:58:33 -0300

    se realizo lista de Socios morosos

--- PI_CLUB_DEPORTIVO/datos/script.sql
+++ PI_CLUB_DEPORTIVO/datos/script.sql
@@ -37,6 +37,7 @@ INSERT INTO cliente (nombre, apellido, dni, correo, telefono, domicilio, fechaAl
 ('Agustina', 'Romero', 35789641, 'agustina.romero@mi.org', '299-3333-0123', 'San Martín 421, Neuquén', '2025-05-12', TRUE, 'socio'),
 ('Santiago', 'Medina', 48215796, 'santiago.medina@su.com', '336-4444-5678', 'Belgrano 987, San Nicolás', '2025-05-14', TRUE, 'no_socio');
 
+--crear tabla de socio
 CREATE TABLE socio (
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	cliente_id INT NOT NULL,
@@ -45,7 +46,7 @@ CREATE TABLE socio (
 	FOREIGN KEY (cliente_id) REFERENCES cliente(id)
 );
 
--- Insertar datos
+-- Insertar datos de socio
 INSERT INTO socio (cliente_id, fechaVencimiento, estado) VALUES
 (1, DATE_ADD(CURDATE(), INTERVAL 5 DAY), 'activo'),
 (2, CURDATE(), 'activo'),
@@ -55,7 +56,7 @@ INSERT INTO socio (cliente_id, fechaVencimiento, estado) VALUES
 (8, DATE_SUB(CURDATE(), INTERVAL 30 DAY), 'inactivo'),
 (9, DATE_ADD(CURDATE(), INTERVAL 30 DAY), 'activo');
 
-
+--crear tabla de cuota
 CREATE TABLE cuota (
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	cliente_id INT NOT NULL,
@@ -68,7 +69,7 @@ CREATE TABLE cuota (
 	FOREIGN KEY (cliente_id) REFERENCES cliente(id)
 );
 
-
+-- Insertar datos de cuotas
 INSERT INTO cuota (cliente_id, fechaVencimiento, fechaPago, monto, formaPago, promocion, estado) VALUES
 -- Cuotas pagadas antes del vencimiento
 (1, DATE_ADD(CURDATE(), INTERVAL 5 DAY), DATE_ADD(CURDATE(), INTERVAL 5 DAY), 1500.00, 'efectivo', 1, 'pagada'),
@@ -85,6 +86,71 @@ INSERT INTO cuota (cliente_id, fechaVencimiento, fechaPago, monto, formaPago, pr
 (8, '2025-07-05', '2025-07-06', 2500.00, 'tarjeta', 2, 'pagada');
 
 
+
+-- CREAR TABLA DE ACTIVIDADES
+CREATE TABLE IF NOT EXISTS actividad (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100),
+    precio FLOAT
+);
+
+
+-- Insertando datos de  Actividades
+INSERT INTO actividad (nombre, precio) VALUES
+('Zumba', 1200.00),
+('Yoga', 1500.00),
+('Crossfit', 2000.00),
+('Funcional', 1800.00),
+('Pilates', 1600.00),
+('Spinning', 1700.00),
+('Boxeo', 1900.00);
+
+
+-- CREAR TABLA DE PAGOS DE ACTIVIDAD (NO SOCIOS)
+CREATE TABLE IF NOT EXISTS pagoactividad (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    idActividad INT,
+    idCliente INT,
+    fechaPago DATE,
+    formaPago ENUM('efectivo', 'tarjeta'),
+    FOREIGN KEY (idActividad) REFERENCES actividad(id),
+    FOREIGN KEY (idCliente) REFERENCES cliente(id)
+);
+
+
+--INSERTAR DATOS DE PAGO DE ACTIVIDAD
+INSERT INTO pagoactividad (idActividad, idCliente, fechaPago, formaPago) VALUES
+(1, 3, '2024-12-01', 'efectivo'),
+(2, 6, '2025-01-10', 'tarjeta'),
+(3, 10, '2025-02-15', 'efectivo'),
+(4, 3, '2025-03-12', 'tarjeta'),
+(5, 6, '2025-04-18', 'efectivo');
+
+
+
+-- CREAR TABLA DE CARNET (SOCIOS)
+CREATE TABLE IF NOT EXISTS carnet (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    idSocio INT,
+    fechaEmision DATE,
+    fechaVencimiento DATE,
+    FOREIGN KEY (idSocio) REFERENCES socio(id)
+);
+
+
+
+
+-- Insertar carnets
+INSERT INTO carnet (idSocio, fechaEmision, fechaVencimiento) VALUES
+(1, '2024-06-15', '2025-06-15'),  -- Ana
+(2, '2024-09-01', '2025-09-01'),  -- Carlos
+(3, '2025-01-20', '2026-01-20'),  -- Juan
+(4, '2025-03-10', '2026-03-10');  -- Sofía
+
+
+
+
+-------------------------------------------------------------store procedure---------------------------------------------------------------------------------
 
 -- store procedure: INGRESO LOGIN
 
@@ -160,6 +226,51 @@ SET @x = 0;
 CALL NuevoCliente('Sofía', 'Fernández', 38741250, 'sofia.fernandez@suemail.com', '381-8888-9012', 'Barrio Nuevo 321, Tucumán', CURDATE(), TRUE, 'socio', @x);
 
 SELECT @x AS respuesta;
+
+
+----------------------------------------------------- store procedure socios con cuotas vencidas-----------------------------------------------------------
+
+DELIMITER //
+
+CREATE PROCEDURE obtener_socios_inactivos_vencidos()
+BEGIN
+    SELECT 
+        c.id AS id_cliente, 
+        c.nombre, 
+        c.apellido, 
+        CASE 
+            WHEN q.estado = 'vencida' AND q.fechaPago IS NULL THEN 'inactivo'
+            ELSE 'activo'
+        END AS estado_socio, 
+        c.dni, 
+        c.correo, 
+        c.telefono, 
+        q.fechaVencimiento AS fecha_vencimiento_cuota,
+        DATEDIFF(CURDATE(), q.fechaVencimiento) AS dias_vencidos_cuota
+    FROM 
+        cliente c
+    JOIN 
+        cuota q ON c.id = q.cliente_id
+    WHERE 
+        q.estado = 'vencida'  -- Solo cuotas vencidas
+        AND q.fechaVencimiento < CURDATE()  -- La cuota ya está vencida
+        AND q.fechaPago IS NULL;  -- La cuota no ha sido pagada
+END //
+
+DELIMITER ;
+
+
+-- Para ejecutar el Stored Procedure y obtener los resultados, simplemente ejecutas el siguiente comando:
+
+CALL obtener_socios_inactivos_vencidos();
+
+
+--------------------------------------------------------------VIStas DE NO_SOCIO--------------------------------------
+
+CREATE VIEW vista_nosocios AS
+SELECT *
+FROM cliente
+WHERE tipoCliente = 'no_socio';
 
 
 
---
+++ PI_CLUB_DEPORTIVO/vistas/listaSociosInactivos/socioInactivo.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace PI_CLUB_DEPORTIVO.vistas.listaSociosInactivos
+{
+    partial class socioInactivo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            cuotaBindingSource = new BindingSource(components);
+            dataGridView1 = new DataGridView();
+            label1 = new Label();
+            ((System.ComponentModel.ISupportInitialize)cuotaBindingSource).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // cuotaBindingSource
+            // 
+            cuotaBindingSource.DataSource = typeof(entidades.Cuota);
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows = false;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            dataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            dataGridView1.BackgroundColor = Color.FromArgb(64, 71, 100);
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(96, 137);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.ReadOnly = true;
+            dataGridView1.ScrollBars = ScrollBars.None;
+            dataGridView1.Size = new Size(769, 339);
+            dataGridView1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(393, 45);
+            label1.Name = "label1";
+            label1.Size = new Size(134, 15);
+            label1.TabIndex = 3;
+            label1.Text = "Lista de Socios Inactivos";
+            // 
+            // socioInactivo
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(64, 71, 100);
+            ClientSize = new Size(963, 530);
+            Controls.Add(label1);
+            Controls.Add(dataGridView1);
+            Name = "socioInactivo";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Socios Inactivos";
+            ((System.ComponentModel.ISupportInitialize)cuotaBindingSource).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private BindingSource cuotaBindingSource;
+        private DataGridView dataGridView1;
+        private Label label1;
+    }
+}
